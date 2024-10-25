cf5afd1d7ad42a16787eb7552b63493fbba57462
InnocentusLime <innocentuslime.help@gmail.com>
2024-10-25 13:28:52 +0300

    Call service at the start and fix trigger url

--- Practice02/src/task03/task03/main.py
+++ Practice02/src/task03/task03/main.py
@@ -16,18 +16,23 @@ class TopicsNode(Node):
     # Runtime setup
     self.default_response = self.get_parameter("default_string").get_parameter_value().string_value()
     self.provided_service_name = self.get_parameter("service_name").get_parameter_value().string_value()
-    self.called_service = self.create_client(Trigger, "spgc/trigger")
+    self.called_service = self.create_client(Trigger, "/spgc/trigger")
     self.provided_service = self.create_service(Trigger, self.service_name, self.proxy_callback)
+    self.fetched_response = None
     #self.timer = self.create_timer(0.5, self.probe_callback)
+
+		# Query the service
+    if not self.called_service.wait_for_service(timeout_sec=1.0):
+      result = self.called_service.call_async(Trigger.Request())
+      rclpy.spin_until_future_complete(self, result)
+      self.fetched_response = result.result().message
 			
   def proxy_callback(self, request, response):
     response.success = True
     response.message = self.default_response
 
-    if not self.called_service.wait_for_service(timeout_sec=1.0):
-      result = self.called_service.call_async(Trigger.Request())
-      rclpy.spin_until_future_complete(self, result)
-      response.message = result.result().message
+    if self.fetched_response is not None:
+      response.message = self.fetched_response
 
     return response
 
